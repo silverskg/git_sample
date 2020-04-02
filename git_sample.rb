e13db411a014fa780137f2a724f1d0bc628fa9e2
silverskg <chokichoki_gm@i.softbank.jp>
2020-04-02 20:39:22 +0900

    introduceメッソドを追加

--- git_sample.rb
+++ git_sample.rb
@@ -3,26 +3,33 @@ class User
 
   def initialize(**params)
     @name = params[:name]
+    @age = params[:age]
+  end
+
+  def introduce
+    if @age > 20
+      "初めまして、私の名前は#{@name}です"
+    else
+      "僕は#{@name}だよ!!!"
+    end
   end
 end
 
-def build_users(names)
+def build_users(params)
   users = []
-  names.each do |name|
-  users << User.new(name: name)
+  params.each do |params|
+  users << User.new(params)
   end
 
   users
 end
 
 
-names = ["saitou","taira", "yamada"]
+params = [{name: "ike",age: 22}, {name: "daa",age: 28},{name: "saitou",age: 2}]
   
-users = build_users(names)
-
-
+users = build_users(params)
 
 users.each do |user|
 
-  puts user.name
+  puts user.introduce
 end
