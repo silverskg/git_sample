179525f32b965c4f4c7cdb704513380cf1bf023b
silverskg <chokichoki_gm@i.softbank.jp>
2020-04-02 20:47:22 +0900

    adultメッソドの追加 eachを短縮

--- git_sample.rb
+++ git_sample.rb
@@ -7,19 +7,24 @@ class User
   end
 
   def introduce
-    if @age > 20
+    if adult?
       "初めまして、私の名前は#{@name}です"
     else
       "僕は#{@name}だよ!!!"
     end
   end
+
+  private
+
+  def adult?
+    @age >= 20
+  end
 end
 
 def build_users(params)
   users = []
-  params.each do |params|
-  users << User.new(params)
-  end
+
+  params.each {|params| users << User.new(params)}
 
   users
 end
@@ -29,7 +34,4 @@ params = [{name: "ike",age: 22}, {name: "daa",age: 28},{name: "saitou",age: 2}]
   
 users = build_users(params)
 
-users.each do |user|
-
-  puts user.introduce
-end
+users.each {|user|puts user.introduce}
